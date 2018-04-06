d0616563db04514578496b0b509fcca0dbba77c2
tke288 <31257759+tke288@users.noreply.github.com>
2018-04-05 19:04:10 -0500

    Initial loading of mock pages

---
+++ src/pages/Create.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Container from "../components/Container";
+
+class Create extends Component {
+
+  render() {
+    return (
+      <Container style={{ minHeight: "80%" }}>
+        <h1 className="text-center">Become a Mentor</h1>
+    
+      </Container>
+    );
+  }
+}
+
+export default Create;
---
+++ src/pages/Discover.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Discover extends Component {  
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Meet a Mentor!</h1>
+      </div>
+    );
+  }
+}
+
+export default Discover;
