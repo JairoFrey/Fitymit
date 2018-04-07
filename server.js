f46a033cd6f45f14fab5845a179e6f532379eb17
tke288 <31257759+tke288@users.noreply.github.com>
2018-04-07 11:43:18 -0500

    Updated post route,  information; sql database

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-// Solution 2: Customer Relations
-// ===========================================
-
-// Step 1: Created a Customer model in ./models/customer.js
-// Step 2: Updated the Burger model to have a hasOne(models.Customer) relation
-// Step 3: Updated the handlebars to display a customers name if there's a 'Customers' property on the Burger
-// Step 4: Updated queries in burgerController for updating a burger to add the CustomerId
-// Step 5: Updated findAll query  in burger_controller for burgers to "include" the customer
-// Step 6: Updated findAll query in burger_controller to order returned burgers by burger_name.
-
-
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -19,15 +8,27 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(bodyParser())
 
 
 app.post("/api/submit", function(req, res){
-  console.log("hi")
-  console.log(req.body)
+ console.log(req.body)
+   var data = req.body;
+  console.log("user: " + JSON.stringify(data))
+   db.Profile.create(data)
+   .then(function(dbPost) {
+     res.json(dbPost);
+   });
 })
 
 app.get("/api/submit", function(req, res){
@@ -37,6 +38,8 @@ app.get("/api/submit", function(req, res){
 
 // listen on port 3000
 var port = process.env.PORT || 3001;
-db.sequelize.sync({force:true}).then(function() {
-  app.listen(port);
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log("Listening on port " + port)
+  });
 });
