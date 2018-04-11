3f54836f8c223ad9d344a084d990038e9659eb80
tke288 <31257759+tke288@users.noreply.github.com>
2018-04-10 20:36:46 -0500

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
+import LinkedInImg from "../Images/signin.png"
 
 class Login extends Component {  
 
   render() {
+    const authurl = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78d1v8603anrge&redirect_uri=http://localhost:3001/auth&state=987654321&scope=r_basicprofile";
     return (
-      <div>
-        <h1 className="text-center">LOG IN</h1>
-      </div>
+        <div className="Login">
+            <a href={authurl}><img src={LinkedInImg} alt="Sign in with Linkedin"/></a>
+        </div>
     );
-  }
+}
 }
 
 export default Login;
