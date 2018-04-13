747b9689e3590b677972679cf46151df0e185faa
Austin Hammontree <30849233+AJHammontree@users.noreply.github.com>
2018-04-12 19:30:33 -0500

    added info
    added cards for mentors and drop down button for industry names

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -4,10 +4,10 @@ class Discover extends Component {
 
   render() {
     return (
-      <div>
-        <h1 className="text-center">Meet a Mentor!</h1>
-      </div>
+
+      
     );
+    
   }
 }
 
